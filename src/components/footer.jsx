1864b603b7c8290b101ac4321f07b25d4b5940b5
jvstln <173433315+jvstln@users.noreply.github.com>
2024-12-15 17:10:16 +0100

    Update footer social links

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -10,6 +10,7 @@ const links = [
 
 const socialLinks = [
   {
+    name: "facebook",
     icon: (
       <svg
         width={11}
@@ -25,8 +26,10 @@ const socialLinks = [
       </svg>
     ),
     href: "#",
+    hidden: true,
   },
   {
+    name: "x",
     icon: (
       <svg
         width={19}
@@ -42,8 +45,10 @@ const socialLinks = [
       </svg>
     ),
     href: "#",
+    hidden: true,
   },
   {
+    name: "instagram",
     icon: (
       <svg
         width={20}
@@ -58,7 +63,25 @@ const socialLinks = [
         />
       </svg>
     ),
-    href: "#",
+    href: "https://www.instagram.com/estap_techs?igsh=MWZucDgzdnZha2gzdQ%3D%3D&utm_source=qr",
+  },
+  {
+    name: "tiktok",
+    icon: (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width={24}
+        height={24}
+        viewBox="0 0 24 24"
+        fill="none"
+      >
+        <path
+          fill="#111D4D"
+          d="M16.6 5.82s.51.5 0 0A4.28 4.28 0 0 1 15.54 3h-3.09v12.4a2.59 2.59 0 0 1-2.59 2.5c-1.42 0-2.6-1.16-2.6-2.6c0-1.72 1.66-3.01 3.37-2.48V9.66c-3.45-.46-6.47 2.22-6.47 5.64c0 3.33 2.76 5.7 5.69 5.7c3.14 0 5.69-2.55 5.69-5.7V9.01a7.35 7.35 0 0 0 4.3 1.38V7.3s-1.88.09-3.24-1.48"
+        ></path>
+      </svg>
+    ),
+    href: "https://www.tiktok.com/@etsap1?_t=ZM-8sDHk4FcrSg&_r=1",
   },
 ];
 const Footer = () => {
@@ -82,13 +105,18 @@ const Footer = () => {
       </ul>
 
       <ul className="social-links flex gap-4">
-        {socialLinks.map((link, index) => (
-          <motion.li
-            key={link.href + index}
-            whileHover={{ y: -3 }}
-            className="flex justify-center items-center rounded-full size-12 bg-neutral-200 text-blue-400"
-          >
-            <a href={link.href}>{link.icon}</a>
+        {socialLinks.map((link) => (
+          <motion.li key={link.name + link.href} whileHover={{ y: -3 }} layout>
+            {!link.hidden && (
+              <a
+                href={link.href}
+                className="flex justify-center items-center rounded-full size-11 bg-neutral-200 text-[#111D4D]"
+                target="_blank"
+                rel="noreferrer"
+              >
+                {link.icon}
+              </a>
+            )}
           </motion.li>
         ))}
       </ul>
