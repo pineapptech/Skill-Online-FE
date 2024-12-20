28fe8661a277de5c309f9f753096a74ba436c7a1
jvstln <173433315+jvstln@users.noreply.github.com>
2024-12-20 12:23:26 +0100

    Change backend baseUrl to live version (testing)
    This commit is for testing purposes only and is squashable

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_ENDPOINT,
+  // baseURL: process.env.NEXT_PUBLIC_API_ENDPOINT,
+  baseURL: "https://api.etsapsfrica.com/api",
 });
