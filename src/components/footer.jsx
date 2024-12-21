701bfdab59934670d508d183d70e9bf640facca1
jvstln <173433315+jvstln@users.noreply.github.com>
2024-12-21 13:45:00 +0100

    Add and update footer social links

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -25,8 +25,7 @@ const socialLinks = [
         />
       </svg>
     ),
-    href: "#",
-    hidden: true,
+    href: "https://www.facebook.com/profile.php?id=61570505078013&__cft__[0]=AZVjCPh6TmlSi1QYVeluaz89rOCJvrhDOLVCnsFCCOD7ExSkXvcp9q6lcMzgHXurPEsz8EcgJoUzRI0KlM20kFfC52L0hbmWA30nPh_ubrHWMtA_r9COX2dODZyYlk3YGqjNct43iRRQFR3HzTH5eckRnroAxLq9M4R2kusP1vLhlZIbzvj8h7He3JQx_7Y53I6c4zixxaGoy8e6zoCN8GK5&__tn__=-]C%2CP-R",
   },
   {
     name: "x",
@@ -44,8 +43,7 @@ const socialLinks = [
         />
       </svg>
     ),
-    href: "#",
-    hidden: true,
+    href: "https://x.com/etsap_tech?s=11",
   },
   {
     name: "instagram",
@@ -63,7 +61,7 @@ const socialLinks = [
         />
       </svg>
     ),
-    href: "https://www.instagram.com/estap_techs?igsh=MWZucDgzdnZha2gzdQ%3D%3D&utm_source=qr",
+    href: "https://www.instagram.com/etsap_techs",
   },
   {
     name: "tiktok",
@@ -81,7 +79,25 @@ const socialLinks = [
         ></path>
       </svg>
     ),
-    href: "https://www.tiktok.com/@etsap1?_t=ZM-8sDHk4FcrSg&_r=1",
+    href: "https://www.tiktok.com/@etsap_techs",
+  },
+  {
+    name: "linkedin",
+    icon: (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width={24}
+        height={24}
+        viewBox="0 0 24 24"
+      >
+        <path
+          fill="currentColor"
+          d="M19 3a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2zm-.5 15.5v-5.3a3.26 3.26 0 0 0-3.26-3.26c-.85 0-1.84.52-2.32 1.3v-1.11h-2.79v8.37h2.79v-4.93c0-.77.62-1.4 1.39-1.4a1.4 1.4 0 0 1 1.4 1.4v4.93zM6.88 8.56a1.68 1.68 0 0 0 1.68-1.68c0-.93-.75-1.69-1.68-1.69a1.69 1.69 0 0 0-1.69 1.69c0 .93.76 1.68 1.69 1.68m1.39 9.94v-8.37H5.5v8.37z"
+        ></path>
+      </svg>
+    ),
+    href: "#",
+    hidden: true,
   },
 ];
 const Footer = () => {
