062058dd48bad6104d08bc253f1e358d0eeb25bb
jvstln <173433315+jvstln@users.noreply.github.com>
2024-12-27 02:14:07 +0100

    Change tawk.to account to emergingtechskill@gmail.com

--- src/app/providers.jsx
+++ src/app/providers.jsx
@@ -10,7 +10,7 @@ const Providers = ({ children }) => {
       var s1 = document.createElement("script"),
         s0 = document.getElementsByTagName("script")[0];
       s1.async = true;
-      s1.src = "https://embed.tawk.to/676d1ff1af5bfec1dbe232e2/1ig149i91";
+      s1.src = "https://embed.tawk.to/676df55faf5bfec1dbe2ac6c/1ig2od0h4";
       s1.charset = "UTF-8";
       s1.setAttribute("crossorigin", "*");
       s0.parentNode.insertBefore(s1, s0);
